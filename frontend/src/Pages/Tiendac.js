3348092531f36cb5defeb2c7c921407a110b8968
Silvanavalser <valenciasilvana19@gmail.com>
2020-09-29 10:46:54 -0500

    Subí mi parte correspondiente de tiendas

---
+++ frontend/src/Pages/Tiendac.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import './css/Tiendac.css';
+
+
+function Tiendac() {
+  return (
+    <main>
+     <div>
+      <header className="navbar navbar-light bg-light shadow-sm">
+        <section className="navbar-brand border border-dark w-70 m-0 pl-3">
+          {" "}
+          RECAPITO{" "}
+        </section>
+        <section className="navbar-brand text-center border border-dark w-30 m-0">
+          {" "}
+          ALGUIEN{" "}
+        </section>
+      </header>
+    </div>
+    <div class="card-align">
+    <div class="card border-dark mb-3">
+  <div class="card-header">TIENDA</div>
+  <img height="200px" src=" https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRucNXlKcU-27xEw5q0nQ0ozArdLYBFeO-B1g&usqp=CAU" class="card-img-top" alt="..."/>
+  <div class="card-footer bg-transparent border-dark"></div>
+  <a href="#" class="btn btn-primary">Ver</a>
+  
+  <div class="card-body text-dark"> 
+  </div>
+</div>
+    <div class="card border-dark mb-3">
+  <div class="card-header">TIENDA</div>
+  <img height="200px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQHwu4RDEFg0sCUHRKTrpHrlxbr4zMbnmvDhg&usqp=CAU " class="card-img-top" alt="..."/>
+  <div class="card-footer bg-transparent border-dark"></div>
+  <a href="#" class="btn btn-primary">Ver</a>
+  
+  <div class="card-body text-dark"> 
+</div>
+</div>
+    <div class="card border-dark mb-3">
+  <div class="card-header">TIENDA</div>
+  <img height="200px" src="https://www.hakunacafe.com/img/logo2_h.png" class="card-img-top" alt="..."/>
+  <div class="card-footer bg-transparent border-dark"></div>
+  <a href="#" class="btn btn-primary">Ver</a>
+
+  <div class="card-body text-dark"> 
+ </div>
+</div>
+    <div class="card border-dark mb-3">
+  <div class="card-header">TIENDA</div>
+  <img height="200px" src=" https://scontent.fbog11-1.fna.fbcdn.net/v/t1.0-9/115702940_2774115129468197_1061027750144544015_n.jpg?_nc_cat=109&_nc_sid=85a577&_nc_ohc=3lPQ9PGmDQ4AX9Kc4rF&_nc_ht=scontent.fbog11-1.fna&oh=3ffeacc7f93381ddbce671cacf61cc7f&oe=5F987EE4" class="card-img-top" alt="..."/>
+  <div class="card-footer bg-transparent border-dark"></div>
+  <a href="#" class="btn btn-primary">Ver</a>
+  
+  <div class="card-body text-dark"> 
+  </div>
+</div>
+    <div class="card border-dark mb-3">
+  <div class="card-header">TIENDA</div>
+  <img height="200px" src=" https://scontent.fbog10-1.fna.fbcdn.net/v/t1.0-9/40072674_1965809260131767_7429409998846820352_n.jpg?_nc_cat=100&_nc_sid=85a577&_nc_ohc=oBnohjbn0KEAX8ukSw5&_nc_ht=scontent.fbog10-1.fna&oh=5b559f3dbbf4bc149752d23a2bd954f7&oe=5F968D49 " class="card-img-top" alt="..."/>
+  <div class="card-footer bg-transparent border-dark"></div>
+  <a href="#" class="btn btn-primary">Ver</a>
+  <div class="card-body text-dark"> 
+  </div>
+</div>
+    <div class="card border-dark mb-3">
+  <div class="card-header">TIENDA</div>
+  <img height="200px" src="https://scontent.fbog11-1.fna.fbcdn.net/v/t1.0-9/s960x960/67348695_769920700072415_220128086216146944_o.jpg?_nc_cat=111&_nc_sid=85a577&_nc_ohc=WJ_1FHnWlCMAX_7ZnLS&_nc_ht=scontent.fbog11-1.fna&tp=7&oh=8f99d5c00e624bee89e011f90b206c37&oe=5F96F446 " class="card-img-top" alt="..."/>
+  <div class="card-footer bg-transparent border-dark"></div>
+  <a href="#" class="btn btn-primary">Ver</a>
+  <div class="card-body text-dark"> 
+  </div>
+</div>
+</div>
+
+
+    </main>
+  );
+}
+
+export default Tiendac;
